61e203699c5c94576f08b0c876b6182f504f6d3b
john-jeffrey-J4 <123716538+john-jeffrey-J4@users.noreply.github.com>
2024-01-30 22:51:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
-const express = require('express');const app = express();
+const express = require('express'); const app = express();
 const port = 3000; // You can change this to any available port you prefer
 
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.send('Hello from your Node.js app!');
 });
 
